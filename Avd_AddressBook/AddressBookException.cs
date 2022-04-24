8d46c39d5dedc5e5008ba8e7c1efa9dd62cab88f
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-24 22:24:51 +0530

    checkout connection

---
+++ Avd_AddressBook/AddressBookException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avd_AddressBook
+{
+    public class AddressBookException : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {           
+            Connection_Failed
+        }
+        public AddressBookException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ Avd_AddressBook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+namespace Avd_AddressBook
+{
+    public class AddressBook
+    {
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = Address_Book_Service; Integrated Security=SSPI;";
+        static SqlConnection connection = new SqlConnection(connectionString);
+        //UC-1 Check connection
+        public static void EstablishConnection()
+        {
+            if (connection != null && connection.State.Equals(ConnectionState.Closed))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    throw new AddressBookException(AddressBookException.ExceptionType.Connection_Failed, "Connection failed..");
+                }
+            }
+            if (connection != null && connection.State.Equals(ConnectionState.Open))
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (Exception)
+                {
+                    throw new AddressBookException(AddressBookException.ExceptionType.Connection_Failed, "Connection failed..");
+                }
+            }
+        }
+        static void Main(string[]args)
+        {
+            AddressBook.EstablishConnection();
+            
+        }
+    }
+}
